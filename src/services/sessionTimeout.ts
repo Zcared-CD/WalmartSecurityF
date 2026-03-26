8a9420baeff065fbfd9c641ef911ffd1461d86db
Carlos Escandon <slashhcarlos106@gmail.com>
2026-03-26 03:12:04 -0600

    Bajada

--- src/services/sessionTimeout.ts
+++ src/services/sessionTimeout.ts
@@ -3,6 +3,25 @@ import api from "@/services/api"
 let timeout: any
 let active = false
 
+const events = ["mousemove", "keydown", "click", "scroll"]
+
+const reset = () => {
+    if (!active) return
+
+    clearTimeout(timeout)
+
+    timeout = setTimeout(() => {
+        console.warn("Sesión expirada por inactividad")
+        logoutUser()
+    }, 1 * 60 * 1000)
+}
+
+const removeListeners = () => {
+    events.forEach(event => {
+        window.removeEventListener(event, reset)
+    })
+}
+
 const logoutUser = async () => {
     try {
         await api.post("/api/logout/")
@@ -16,25 +35,12 @@ const logoutUser = async () => {
 export const stopSessionTimeout = () => {
     active = false
     clearTimeout(timeout)
+    removeListeners()
 }
 
 export const initSessionTimeout = () => {
-
     if (active) return
     active = true
-
-    const events = ["mousemove", "keydown", "click", "scroll"]
-
-    const reset = () => {
-        if (!active) return
-
-        clearTimeout(timeout)
-
-        timeout = setTimeout(() => {
-            console.warn("Sesión expirada por inactividad")
-            logoutUser()
-        }, 1 * 60 * 1000)
-    }
 
     events.forEach(event => {
         window.addEventListener(event, reset)
